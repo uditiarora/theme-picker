7c5dea237c0e5a462fd90e15ce3dd7f1028f7626
Uditi <uditiarora@gmail.com>
2020-03-21 20:53:44 +0530

    Updates

---
+++ precache-manifest.9ed55959e87e912c1a580c1bf357dd4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a658357846c66e86aaf9d012b10bd964",
+    "url": "/theme-picker/index.html"
+  },
+  {
+    "revision": "a4b45fa7e4b48bb17027",
+    "url": "/theme-picker/static/css/main.cfb9ba3d.chunk.css"
+  },
+  {
+    "revision": "c7f4e3cbe4c2dd027116",
+    "url": "/theme-picker/static/js/2.d760b9e4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/theme-picker/static/js/2.d760b9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b45fa7e4b48bb17027",
+    "url": "/theme-picker/static/js/main.8c72a84f.chunk.js"
+  },
+  {
+    "revision": "1202e9ee520e31ff9029",
+    "url": "/theme-picker/static/js/runtime-main.fcb7a79b.js"
+  }
+]);
