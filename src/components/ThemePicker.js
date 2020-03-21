7f80d673a1a937d27a30b127562671890d774a48
Uditi <uditiarora@gmail.com>
2020-03-21 13:10:05 +0530

    so far so good

--- src/components/ThemePicker.js
+++ src/components/ThemePicker.js
@@ -1,24 +1,45 @@
 import React from 'react';
 import '../css/ThemePicker.css';
 import ColorPicker from './ColorPicker';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import * as Scroll from 'react-scroll';
+import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 function ThemePicker() {
+   function handleSetActive(to) {
+        console.log(to);
+    };
     return(
-        <div className = "main ">
-           <div className = "container-fluid">
-            <div>
-                <p class="text1 script"><span>Theme Picker</span></p>
-                <p class="script"><span>5 colors are all you need!</span></p>
+        <div>
+            <div className = "main ">
+            <div className = "container-fluid">
+                <div>
+                    <p className="text1 script"><span>Theme Picker</span></p>
+                    <p className="script"><span>5 colors are all you need !</span></p>
+                </div>
+                <br></br>
+                <div className="row row-flex">
+                        <ColorPicker />
+                        <ColorPicker />
+                        <ColorPicker />
+                        <ColorPicker />
+                        <ColorPicker />
+                </div>
+                <div>
+                    <p className="script"><span>Scroll down to see some of our themes</span></p>   
+                    <Link activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={handleSetActive}>
+                        <div  className="scroll-down"><ExpandMoreIcon className = "my_icon" style={{ fontSize: 80 }}/> </div>
+                    </Link>
+                    
+                </div>    
+            </div>            
             </div>
-            <div className="row row-flex">
-                    <ColorPicker />
-                    <ColorPicker />
-                    <ColorPicker />
-                    <ColorPicker />
-                    <ColorPicker />
-               </div>
-                
-           </div>            
+            <div>
+            <Element name="test1" >
+            <div > </div>
+            </Element>
+            </div>
         </div>
+            
     );
 }
 export default ThemePicker;
